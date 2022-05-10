d8b26aaaa48001f04e2efe23497c1745fa0def94
Guilherme <guilhermecsilva8@gmail.com>
2022-05-10 16:07:01 -0300

    Implementação do MediaPlayer.

--- app/src/main/java/com/example/sonsparaestudar/MainActivity.kt
+++ app/src/main/java/com/example/sonsparaestudar/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.sonsparaestudar
 
+import android.annotation.SuppressLint
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
+import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.example.sonsparaestudar.adapters.ViewPagerAdapter
+import com.example.sonsparaestudar.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var mp: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val tabLayout = findViewById<TabLayout>(R.id.tlAbas)
         val viewPager2 = findViewById<ViewPager2>(R.id.vpFragments)
@@ -49,15 +61,75 @@ class MainActivity : AppCompatActivity() {
         val music = intent.getParcelableExtra<Music>("music")
         val tab = intent.getIntExtra("tab", 0)
         tabLayout.getTabAt(tab)?.select()
-        Toast.makeText(this, "Selected ${music?.title} from tab $tab", Toast.LENGTH_SHORT).show()
 
         if(music != null) {
-            //controlSound(music.song)
+            controlSound(music.song)
         }
+    }
+
+    private fun controlSound(id: Int) {
+        var isPlaying = true
+        binding.fabPlayPause.setImageResource(R.drawable.ic_pause)
+
+        if(mp !== null) {
+            mp?.stop()
+            mp?.reset()
+            mp?.release()
+            mp = null
+        }
+
+        mp = MediaPlayer.create(this, id)
+        initialiseSeekBar()
+        mp?.start()
+
+        binding.fabPlayPause.setOnClickListener {
+            if(isPlaying) {
+                if(mp !== null) mp?.pause()
+                binding.fabPlayPause.setImageResource(R.drawable.ic_play)
+                isPlaying = false
+            } else {
+                mp?.start()
+                binding.fabPlayPause.setImageResource(R.drawable.ic_pause)
+                isPlaying = true
+            }
+        }
+
+        binding.sbTime.setOnSeekBarChangeListener(@SuppressLint("AppCompatCustomView")
+        object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if(fromUser) mp?.seekTo(progress)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+    }
+
+    private fun initialiseSeekBar() {
+        binding.sbTime.max = mp!!.duration
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object: Runnable{
+            override fun run() {
+                try {
+                    binding.sbTime.progress = mp!!.currentPosition
+                    handler.postDelayed(this, 1000)
+                } catch (e: Exception) {
+                    binding.sbTime.progress = 0
+                }
+            }
+        }, 0)
     }
 
     override fun onStop() {
         super.onStop()
 
+        mp?.stop()
+        mp?.reset()
+        mp?.release()
+        mp = null
     }
 }
--- app/src/main/java/com/example/sonsparaestudar/adapters/MusicAdapter.kt
+++ app/src/main/java/com/example/sonsparaestudar/adapters/MusicAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.sonsparaestudar.adapters
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sonsparaestudar.Music
 import com.example.sonsparaestudar.R
